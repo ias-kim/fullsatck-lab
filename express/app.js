634fb1f4f07d5712aa3ac5bac35b9867fac038de
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-03 00:00:07 +0900

    Add Promise callback Code

--- express/app.js
+++ express/app.js
@@ -1,27 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const { readFile } = require('fs');
 
-const path = require('path');
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile('./content/first.txt', 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-app.post('/', (req, res) => {
-  res.send('Got a Post request!');
-});
-
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user');
-});
-
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+getText('./content/second.txt');
