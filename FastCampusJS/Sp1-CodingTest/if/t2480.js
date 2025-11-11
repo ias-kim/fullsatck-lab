8a352702a38e12f1f3d72f3c5bef03b424323f24
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-11-11 19:02:43 +0900

    코딩테스트 조건문 풀이

--- FastCampusJS/Sp1-CodingTest/if/t2480.js
+++ FastCampusJS/Sp1-CodingTest/if/t2480.js
@@ -5,9 +5,9 @@ let a = Number(input[0].split(' ')[0]);
 let b = Number(input[0].split(' ')[1]);
 let c = Number(input[0].split(' ')[2]);
 
-if (a === b && c === b) {
+if (a == b && c == b) {
   console.log(10000 * a * 1000);
-} else if (a == b) console.log(10000 * a * 1000);
-else if (a == c) console.log(10000 * a * 1000);
-else if (b == c) console.log(10000 * b * 1000);
+} else if (a == b) console.log(1000 * a * 100);
+else if (a == c) console.log(1000 * a * 100);
+else if (b == c) console.log(1000 * b * 100);
 else console.log(Math.max(a, b, c) * 100);
